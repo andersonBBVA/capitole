8c28744f2191bb659499477175fb6d5acc2dbd2a
jandersoncasas <jandersoncasas@gmail.com>
2023-08-29 23:07:17 +0200

    Req-Dev: spring initializr

---
+++ electronic-commerce/src/test/java/com/capitole/electroniccommerce/ElectronicCommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.electroniccommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectronicCommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
